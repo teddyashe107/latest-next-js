e293b8543530148a8214d4f6c604611b1f14a3fd
teddyashe107 <teddyashe107@gmail.com>
2023-03-13 20:27:48 +0300

    ci: implement commitlint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,10 @@
 import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
-import styles from '@/styles/Home.module.css';
 import Button from '@/components/atoms/Button';
 import dynamic from 'next/dynamic';
 
 const ThemeSwitch = dynamic(() => import('@/components/atoms/ThemeSwitch'), {
   ssr: false,
 });
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   // variadic function in typescript
