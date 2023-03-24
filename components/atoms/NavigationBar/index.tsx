5780543e444631d5f9013994c792b966c2c3f35d
teddyashe107 <teddyashe107@gmail.com>
2023-03-24 17:49:29 +0300

    refactor: refactor navigation bar component

--- components/atoms/NavigationBar/index.tsx
+++ components/atoms/NavigationBar/index.tsx
@@ -7,11 +7,11 @@ const navigationBarStyles = cva('mt-4 md:mt-0 md:flex gap-x-8', {
       vertical: 'flex flex-col',
     },
     active: {
-      home: 'text-rose-700 bg-yellow-700'
+      home: 'text-rose-700 bg-yellow-700',
     },
     hidden: {
-      true: 'hidden'
-    }
+      true: 'hidden',
+    },
   },
   compoundVariants: [],
   defaultVariants: {},
@@ -19,29 +19,34 @@ const navigationBarStyles = cva('mt-4 md:mt-0 md:flex gap-x-8', {
 
 const ListItem = ['home', 'blog', 'about'];
 
-export interface INavigationBar
+export interface NavigationBarProps
   extends VariantProps<typeof navigationBarStyles> {}
 
-const NavigationBar: React.FC<INavigationBar> = ({ variant, hidden }) => {
-
-  const [active, setActive] = useState(0);
+const NavigationBar: React.FC<NavigationBarProps> = ({ variant, hidden }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
 
   function handleClick(item: number) {
-    console.log('teddy clicked', item)
-    setActive(item)
+    setActiveIndex(item);
   }
+
+  const renderListItems = () =>
+    ListItem.map((item, index) => (
+      <li
+        key={index}
+        className={`${
+          activeIndex === index
+            ? 'text-rose-700 dark:text-rose-700'
+            : 'text-gray-700'
+        } py-2 md:py-0 hover:text-gray-900 dark:text-gray-100 dark:hover:text-rose-700 cursor-pointer md:border-b`}
+        onClick={() => handleClick(index)}
+      >
+        {item}
+      </li>
+    ));
 
   return (
     <ul className={navigationBarStyles({ variant, hidden })}>
-      {ListItem.map((item, index) => (
-        <li
-          key={index}
-          className={`${active === index ? 'text-rose-700 dark:text-rose-700' : 'text-gray-700'} py-2 md:py-0  hover:text-gray-900 dark:text-gray-100 dark:hover:text-rose-700 cursor-pointer md:border-b`}
-          onClick={() => handleClick(index)}
-        >
-          {item}
-        </li>
-      ))}
+      {renderListItems()}
     </ul>
   );
 };
